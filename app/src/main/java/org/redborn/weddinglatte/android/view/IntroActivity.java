cb5037da77c2e03250428a621a0525b514505ad4
jjj <jjjsins@gmail.com>
2016-05-10 15:57:11 +0900

    웨딩라떼 런처 화면 추가 feat(view) : add supplier activity and layout

---
+++ app/src/main/java/org/redborn/weddinglatte/android/view/IntroActivity.java
@@ -0,0 +1,15 @@
+package org.redborn.weddinglatte.android.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.redborn.weddinglatte.R;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
