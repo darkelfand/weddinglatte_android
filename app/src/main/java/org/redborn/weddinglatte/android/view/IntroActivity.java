0602931366e807bf7835b0d6516aa157a8e56c53
jjj <jjjsins@gmail.com>
2016-06-20 16:47:01 +0900

    feat(view) : add supplier detail activity
    웨딩하떼 화면 추가

--- app/src/main/java/org/redborn/weddinglatte/android/view/IntroActivity.java
+++ app/src/main/java/org/redborn/weddinglatte/android/view/IntroActivity.java
@@ -1,7 +1,9 @@
 package org.redborn.weddinglatte.android.view;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
 import org.redborn.weddinglatte.R;
 
@@ -11,5 +13,15 @@ public class IntroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        Handler handler  = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                //Intent intent = new Intent(IntroActivity.this, LoginActivity.class);
+                Intent intent = new Intent(IntroActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
     }
 }
