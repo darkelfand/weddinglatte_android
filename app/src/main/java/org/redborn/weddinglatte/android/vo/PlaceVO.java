96523cc8a7b6465c5160f97ff32225ce8bb4adba
jjj <jjjsins@gmail.com>
2016-06-20 11:05:30 +0900

    feat(view) : add supplier vo
    업체 리스트 화면을 구성하는 item vo 추가

---
+++ app/src/main/java/org/redborn/weddinglatte/android/vo/PlaceVO.java
@@ -0,0 +1,10 @@
+package org.redborn.weddinglatte.android.vo;
+
+    public class PlaceVO{
+
+    public String name;
+    public String vicinity;
+    public double lat;
+    public double lng;
+
+}
